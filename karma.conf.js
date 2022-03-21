249a279a6ae5c05bf49640945619a0fd94e83a67
Robbin Schmidt <robbinschmidt@gmail.com>
2022-03-21 17:19:53 +0100

    store

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-playground'),
+      dir: require('path').join(__dirname, './coverage/angular-store-test'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormGroup, FormControl } from '@angular/forms';
-import { ColorPaletteComponent } from './components/color-palette/color-palette.component';
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { CounterComponent } from './counter/counter.component';
+
 @NgModule({
-    declarations: [
-        AppComponent,
-        ColorPaletteComponent
-    ],
-    imports: [
-        BrowserModule,
-        MatTabsModule,
-        MatDatepickerModule,
-        MatSliderModule,
-        AppRoutingModule,
-        BrowserAnimationsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    CounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({count: counterReducer})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
-export class AppModule {
-    campaignOne: FormGroup;
-    campaignTwo: FormGroup;
-
-    constructor() {
-        const today = new Date();
-        const month = today.getMonth();
-        const year = today.getFullYear();
-
-        this.campaignOne = new FormGroup({
-            start: new FormControl(new Date(year, month, 13)),
-            end: new FormControl(new Date(year, month, 16))
-        });
-
-        this.campaignTwo = new FormGroup({
-            start: new FormControl(new Date(year, month, 15)),
-            end: new FormControl(new Date(year, month, 19))
-        });
-    }
-}
+export class AppModule { }
